4b76d6c04234afd65b3f1e767ab8db4840f615a1
Nguyễn Văn Hưởng <>
2020-09-08 11:05:39 +0700

    updated

--- 1. Relational Database/customers.sql
+++ 1. Relational Database/customers.sql
@@ -1,4 +1,12 @@
+create database customers;
+create table data
+(id varchar(10),
+name varchar(30));
+select * from data;
+select id, name from customers;
+select * from customers where name='Nguyen A';
 select * from data where name like '%N%';
+select * from data where id in ('01', '02');
 
 
 
